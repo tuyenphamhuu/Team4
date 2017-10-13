a1724e18374f183e222fcca5be7b8d3d03982099
NhungNTH95 <nhung.nth@neo-lab.vn>
2017-10-13 09:24:27 +0700

    not change

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -13,29 +13,7 @@
 					            <img class="mx-auto d-block rounded" src="images/ip-x-f.jpg" alt="">
 					            <div class="overlay">
 					                <h2>iPhone X</h2>
-					                <div class="dropdown">
-							  			<!-- Button trigger modal -->
-										<button id="myBtn" type="button" class="info btn btn-primary" data-toggle="modal" data-target="#exampleModal">View Detail
-										</button>
-										<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-										  <div class="modal-dialog" role="document">
-										    <div class="modal-content">
-										      <div class="modal-header">
-										        <h5 class="modal-title" id="exampleModalLabel">Iphone 8</h5>
-										      </div>
-										      <div class="modal-body">
-										        <p><b>Screen size: <small>5.8 inch</small></b></p>
-												<p><b>Internal Storage: <small>64/256GB</small></b></p>
-												<p><b>Front-facing cam: <small>7MB TrueDepth f/22</small></b></p>
-												<p><b>Video Capture: <small>4K at 60fps</small></b></p>
-												<p><b>Bluetooth <small>v5.0</small></b></p>
-												<p><b>Oprerating System <small>ios11</small></b></p>
-												<p><b>Wifi: <small>Dual band 802.11ac</small></b></p>
-										      </div>
-										    </div>
-										  </div>
-										</div>
-									</div>		
+					               	
 					            </div>
 					        </div>
 					    </div>
@@ -178,6 +156,43 @@
 		<img src="images/imacbg-f.jpg">
 		<div><strong>iMac Pro</strong><span>(Power to the pro)</span></div>
 	</div>
+	<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  View More
+</button>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Iphone 8</h5>
+       
+      </div>
+      <div class="modal-body">
+        <p>
+							<b>Screen size: <small>5.8 inch</small></b>
+						</p>
+						<p>
+							<b>Internal Storage: <small>64/256GB</small></b>
+						</p>
+						<p>
+							<b>Front-facing cam: <small>7MB TrueDepth f/22</small></b>
+						</p>
+						<p>
+							<b>Video Capture: <small>4K at 60fps</small></b>
+						</p>
+						<p>
+							<b>Bluetooth <small>v5.0</small></b>
+						</p>
+						<p>
+							<b>Oprerating System <small>ios11</small></b>
+						</p>
+						<p>
+							<b>Wifi: <small>Dual band 802.11ac</small></b>
+						</p>
+      </div>
+    </div>
+  </div>
+</div>
 
 </content>
 
