a3a608a7d8c959af49260b937be90513d016192a
PhiVuNguyen2010 <vu.np@neo-lab.vn>
2017-10-13 10:26:35 +0700

    + Add product controller update

---
+++ app/views/admin/editProduct.view.php
@@ -0,0 +1,74 @@
+
+<?php require 'app/views/partials/head.php'; ?>
+<?php require 'app/views/partials/head-admin.php'; ?>
+<br>
+<div class="container-fluid">
+    <div class="row">
+        <?php require 'app/views/partials/sidebar-left.php'; ?>
+        
+        <div class="right col-lg-10 col-md-10 col-sm-8">
+            <form id="form-func" action="/editProduct" method="POST">
+            <input name="ID_Product" type="text" value="<?php echo $product[0]->ID_Product;?>" hidden="hidden">
+                <div class="form-group row">
+                    <div class="col-md-3">
+                        <label for="exampleInputEmail1">Product name</label>
+                    </div>
+                    <div class="col-md-9">
+                        <input name="ProductName" value="<?php echo $product[0]->ProductName;?>" type="text"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Product name">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-3">
+                        <label for="exampleInputEmail1">New Price</label>
+                    </div>
+                    <div class="col-md-9">
+                        <input name="NewPrice" value="<?php echo $product[0]->NewPrice;?>" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter New Price">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-3">
+                        <label for="exampleInputEmail1">Old Price</label>
+                    </div>
+                    <div class="col-md-9">
+                        <input name="OldPrice" value="<?php echo $product[0]->OldPrice;?>" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Old Price">
+                    </div>
+                </div>
+                <!-- <div class="form-group row">
+                    <div class="col-md-3">
+                        <label for="exampleInputEmail1">ID_Type Product</label>
+                    </div>
+                    <div class="col-md-9">
+                        <input name="" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter ID_Type Product">
+                    </div>
+                </div> -->
+                <div class="form-group row">
+                    <div class="col-md-3">
+                        <label for="exampleInputEmail1">Color Product</label>
+                    </div>
+                    <div class="col-md-9">
+                        <input name="ColorProduct" value="<?php echo $product[0]->ColorProduct;?>" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Color Product">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-3">
+                        <label for="exampleInputEmail1">Config</label>
+                    </div>
+                    <div class="col-md-9">
+                        <textarea  name="Config" name="message" rows="5" cols="100"><?php echo $product[0]->Config;?>
+                        </textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-3">
+                        <label for="exampleInputEmail1">Description</label>
+                    </div>
+                    <div class="col-md-9">
+                    <textarea  name="description" name="message" rows="5" cols="100"><?php echo $product[0]->description;?>
+                    </textarea>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Add</button>
+            </form>
+        </div>
+    </div>
+</div>
