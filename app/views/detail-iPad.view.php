51bf1249706899167a975eb0a94b16ef8de3609a
Hang Vo <hang.vt@neolab.vn>
2017-10-16 10:17:20 +0700

    *fix ipad detail-color-connectivity-quantity

--- app/views/detail-iPad.view.php
+++ app/views/detail-iPad.view.php
@@ -32,49 +32,91 @@
 					      	<div id="collapse1" class="panel-collapse collapse in">
 					        	<div class="panel-body">
 					        		<div class="row">
-					        		<div class="col-lg-3 text-center">
-						        		<a href="#">
-						        			<!-- <button type="button" class="btn  btn-outline-secondary"> -->
+					        			<div class="col-lg-3 text-center">
+						        		<!-- <a href="#">
+						        			<button type="button" class="btn  btn-outline-secondary">
 						        				<div class="button color">
 						        				<img src="public/images/color-silver-201508.png">
 						        				<p>Silver</p>
 						        				</div>
 						        		
-						        			<!-- </button> -->
-						        		</a>
-						        	</div>
-						        	<div class="col-lg-3 text-center">
-						        		<a href="#">
-						        			<!-- <button type="button" class="btn btn-outline-secondary"> -->
+						        			</button>
+						        		</a> -->
+
+						        			<div class="form-check">
+										    	<input class="form-check-input" type="radio" name="Radios1" id="exampleRadios1" value="option1" checked>
+										    		<label for="exampleRadios1" class="form-check-label">
+										     		<span>
+										    			<img src="public/images/color-silver-201508.png">
+										    			<p>Silver</p>
+										    		</span>
+										  		</label>	 
+											</div>
+
+						        		</div>
+						        		<div class="col-lg-3 text-center">
+						        		<!-- <a href="#">
+						        			<button type="button" class="btn btn-outline-secondary">
 						        				<img src="public/images/color-space-gray-201508.png">
 						        				<p>Space Gray</p>
 						        		
-						        			<!-- </button> -->
-						        		</a>
-						        	</div>
-						        	<div class="col-lg-3 text-center">
-						        		<a href="#">
+						        			</button>
+						        		</a> -->
+						        			<div class="form-check">
+										    	<input class="form-check-input" type="radio" name="Radios1" id="exampleRadios2" value="option1" checked>
+										     	<label for="exampleRadios2" class="form-check-label">
+										     		<span>
+										    			<img src="public/images/color-space-gray-201508.png">
+										    			<p>Space Gray</p>
+										    		</span>
+										  		</label>
+											</div>
+						        		</div>
+						        		<div class="col-lg-3 text-center">
+						        		<!-- <a href="#">
 						        			<button type="button" class="btn btn-outline-secondary">
-						        				<img src="public/images/color-gold-201508.png">
+						        				<img src="public/images/color-space-gray-201508.png">
 						        				<p>Gold</p>
 						        		
 						        			</button>
-						        		</a>
-						        	</div>
-						        	<div class="col-lg-3 text-center">
-						        		<a href="#">
+						        		</a> -->
+						        			<div class="form-check">
+										    	<input class="form-check-input" type="radio" name="Radios1" id="exampleRadios3" value="option1" checked>
+										     	<label for="exampleRadios3" class="form-check-label">
+										     		<span>
+										    		<img src="public/images/color-gold-201508.png">
+										    		<p>Gold</p>
+										    		</span>
+										  		</label>
+											</div>
+						        		</div>
+						        		<div class="col-lg-3 text-center">
+						        		<!-- <a href="#">
 						        			<button type="button" class="btn btn-outline-secondary">
 						        				<img src="public/images/color-rosegold-201509.png">
 						        				<p>Rose Gold</p>
 						        		
 						        			</button>
-						        		</a>
-						        	</div>
-					        	</div>
+						        		</a> -->
+						        			<div class="form-check">
+										    	<input class="form-check-input" type="radio" name="Radios1" id="exampleRadios4" value="option1" checked>
+										     	<label for="exampleRadios4" class="form-check-label">
+										     		<span>
+										    			<img src="public/images/color-rosegold-201509.png">
+										    			<p>Rose Gold</p>
+										    		</span>
+										  		</label>
+											</div>
+						        		</div>
+					        		</div>
 					        	</div>
 					      	</div>
 					    </div>
 					    <hr>
+					</div>
+				</div>
+				<div class="detail3">
+					<div class="panel-group" >
 					    <div class="panel panel-default">
 					      	<div class="panel-heading">
 					        	<h4 class="panel-title">
@@ -85,29 +127,52 @@
 					        	<div class="panel-body">
 					        		<div class="row">
 						        		<div class="col-lg-6 text-center">
-							        		<a href="#">
+							        		<!-- <a href="#">
 							        			<button type="button" class="btn btn-outline-secondary" >
 							        				<p>Wi-Fi</p>
 													<p>$799</p>	
 							        			</button>
-							        		</a>
+							        		</a> -->
+							        		<div class="form-check">
+										    	<input class="form-check-input" type="radio" name="Radios2" id="exampleRadios5" value="option2" checked>
+										    	<label for="exampleRadios5" class="form-check-label">
+										     		<span>
+										    			<p>Wi-Fi</p>
+														<p>$799</p>	
+										    		</span>
+										  		</label>
+											</div>
 							        	</div>
 							        	<div class="col-lg-6 text-center">
-							        		<a href="#">
-							        			<button type="button" class="btn btn-outline-secondary">
-							        					<p>Wi-Fi + Cellular</p>
-														<p>$929</p>	
-							        			</button>
-							        		</a>
+							        		<div class="form-check">
+										    	<input class="form-check-input" type="radio" name="Radios2" id="exampleRadios6" value="option2" checked>
+										    	<label for="exampleRadios6" class="form-check-label">
+										     		<span>
+										    			<p>Wi-Fi + Cellular</p>
+														<p>$929</p>
+										    		</span>
+										  		</label>
+											</div>
 							        	</div>
-						        	
 					        		</div>
 					        	</div>
 					      	</div>
 					    </div>
 					    <hr>
   					</div>
+  				</div>
+  				<div class="detail4 row">
+					 <label for="example-number-input" class="col-3 col-form-label">Quantity</label>
+					 <div class="col-9">
+					    <input class="form-control" type="number" value="1" id="example-number-input">
+					 </div>
 				</div>
+				<hr>
+				<div class="detail5 row">
+    				<a href="#" target="_blank"><button type="button" id="buynow-btn" class="btn btn-dark">Buy now</button></a>
+    				<a href="#" target="_blank"><button type="button" id="add-btn" class="btn btn-dark">Add to cart</button></a>
+				</div>	
+
 			</div>	
 		</div>
 	</div>
