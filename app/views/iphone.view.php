422d48879be6f88fc6b8e199d53ab8ff97870079
PhiVuNguyen2010 <vu.np@neo-lab.vn>
2017-10-17 09:05:18 +0700

    * fix display iphone hover

--- app/views/iphone.view.php
+++ app/views/iphone.view.php
@@ -19,13 +19,20 @@
 				
 							<div class="iphone-item">
 								<a href="#">
+									<div class="overlay-image">
 									<img src="public/images/<?php echo $products->Image; ?>">
+										<div class="overlay">
+											<div class="text">Tech specs</div>
+										</div>
+									</div>
+									
 									<h5><?php echo $products->ProductName; ?></h5>
 									<p><b>From $<?php echo $products->NewPrice; ?></b></p>
 									<a href="#" target="_blank"><button type="button" class="btn btn-dark">Learn more</button></a>
 									<a href="#" target="_blank"><button type="button" class="btn btn-dark">Buy</button></a>
 									
 								</a>
+								
 							</div>
 						
 				
