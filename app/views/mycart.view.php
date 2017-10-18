a7f1f7960616e21005fc6f8e53189d5eb8f6aa4b
NhungNTH95 <nhung.nth@neo-lab.vn>
2017-10-18 23:04:21 +0700

    + Add reponsive div#mac

--- app/views/mycart.view.php
+++ app/views/mycart.view.php
@@ -5,7 +5,7 @@
 	<!-- cart -->
 	<div class="cart">
 		<h2 class="font-weight-bold text-center">My cart</h2>
-		<table class="bg-cart bill-ap table text-transform table-hover table-responsive">
+		<table class="bg-cart bill-ap table table-inverse text-transform table-hover table-responsive">
 			<thead>
 				<tr>
 					<th style="width: 4%">	#</th>
@@ -17,7 +17,7 @@
 				</tr>
 			</thead>
 			<tbody>
-				<tr class="list-pr-ap">
+				<tr class="list-pr-ap align-middle">
 					<th>1</th>
 					<td>
 						<div class="row">
@@ -29,7 +29,7 @@
 							</div>
 						</div>
 					</td>
-					<td>$699</td>
+					<th>$699</th>
 					<td class="count-ap">
 						<input class="text-center" type="number" value="1" min="1" >
 					</td>
@@ -53,7 +53,7 @@
 							</div>
 						</div>
 					</td>
-					<td>$799</td>
+					<th>$799</th>
 					<td class="count-ap">
 						<input class="text-center" type="number" value="2" min="1">
 					</td>
@@ -80,7 +80,7 @@
 							</div>
 						</div>
 					</td>
-					<td>$669</td>
+					<th>$669</th>
 					<td class="count-ap">
 						<input class="text-center" type="number" value="1" min="1">
 					</td>
