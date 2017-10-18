262bf85c108001af6accbc8b7854d8b17da0e3f8
PhiVuNguyen2010 <vu.np@neo-lab.vn>
2017-10-18 09:09:49 +0700

    @ change addProduct and editProduct

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -36,14 +36,18 @@ class ProductController
 
   public function addProduct() 
   {
-    move_uploaded_file($_FILES['Image']['tmp_name'],"public/images/".$_FILES['Image']['name']); 
+    move_uploaded_file($_FILES['Image']['tmp_name'],"public/images/".$_FILES['Image']['name']);
+    if($_FILES['Image']['name']==''){
+      $image='knowledge_graph_logo.png';
+    }else{
+      $image=$_FILES['Image']['name'];
+    }
     Product::insert($_POST['ProductName'],
       $_POST['NewPrice'],
       $_POST['OldPrice'],
-      $_POST['ColorProduct'],
       $_POST['Config'],
       $_POST['description'],
-      $_FILES['Image']['name']
+      $image
     );
 
     return redirect('admin/indexProduct');
@@ -69,16 +73,15 @@ class ProductController
         'ProductName'  => $_POST['ProductName'],
         'NewPrice'     => $_POST['NewPrice'],
         'OldPrice'     => $_POST['OldPrice'],
-        'ColorProduct' => $_POST['ColorProduct'],
         'Config'       => $_POST['Config'],
-        'description'  => $_POST['description']
+        'description'  => $_POST['description'],
+        'Image'        => $_POST['knowledge_graph_logo.png']
         ];
     }else{
       $params = [
     'ProductName'      => $_POST['ProductName'],
     'NewPrice'         => $_POST['NewPrice'],
     'OldPrice'         => $_POST['OldPrice'],
-    'ColorProduct'     => $_POST['ColorProduct'],
     'Config'           => $_POST['Config'],
     'description'      => $_POST['description'],
     'Image'            => $_FILES['Image']['name']
