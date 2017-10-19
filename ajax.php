d3a20e01b7023b5eafb5b49ecf45fbe738d2f982
Phạm Hữu Tuyến <tuyen.ph@neo-lab.vn>
2017-10-19 09:46:52 +0700

    No more

--- ajax.php
+++ ajax.php
@@ -1,25 +1,16 @@
 <?php
 session_start();
 $color = $_REQUEST['color'];//Lay parameter tu request
-$sl = $_REQUEST['sl'];//Lay parameter tu request
-$id = $_REQUEST['id'];//Lay parameter tu request
-
-// $_SESSION['cart']['color']=$color;
-// $_SESSION['cart']['sl']=$sl;
-// $_SESSION['cart']['id']=$id;
-
-
-
- //echo "So luong ".$_SESSION['color'];
+$sl    = $_REQUEST['sl'];  //Lay parameter tu request
+$id    = $_REQUEST['id']; //Lay parameter tu request
 
  $_SESSION['cart']= array(
     $id => array(
         "Color" => $color,
         "Quantity" => $sl
     )
-);
+ );
 echo "<pre>";
-die(var_dump($_SESSION['cart']));
+	die(var_dump($_SESSION['cart']));
 echo "</pre>";
-//echo "mau ne".$_SESSION['cart']['color'];
 ?>
--- app/views/mycart.view.php
+++ app/views/mycart.view.php
@@ -4,8 +4,84 @@
 	<!-- cart -->
 	<div class="cart">
 
-	<?php var_dump($_SESSION['cart']); ?>
+	<?php 
+		echo "<pre>";
+			print_r($_SESSION['cart']);
+		echo "</pre>";
+
+	 ?>
 		<h2 class="font-weight-bold text-center">My cart</h2>
+		<table class="bg-cart bill-ap table table-inverse text-transform table-hover table-responsive">
+			<thead>
+				<tr>
+					<th style="width: 4%">	#</th>
+					<th style="width: 45%">Name Products</th>
+					<th style="width: 15%">Price</th>
+					<th style="width: 15%">Quantity</th>
+					<th style="width: 15%">Total Money</th>
+					<th style="width: 6%">Delete</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="list-pr-ap">
+					<th>3</th>
+					<td>
+						<div class="row">
+							<div class="col-xl-3 col-lg-3 col-md-4 col-sm-12 col-12">
+								<img class="img-myc" src="../../public/images/6+.jpg">
+							</div>
+							<div class="col-xl-9 col-lg-9 col-md-8 col-sm-12 col-12">
+								iPhone 7 plus
+							</div>
+						</div>
+					</td>
+					<th>$669</th>
+					<td class="count-ap">
+						<input class="text-center" type="number" value="1" min="1">
+					</td>
+					<td>$669</td>
+					<td class="edit-bill">
+						<!-- Button trigger modal -->
+						<button type="button" data-toggle="modal" data-target="#exampleModal">
+						  	<img class="remove" src="/public/images/trash-ap-3.png">
+						</button>
+						<!-- Modal -->
+						
+
+					</td>
+				</tr>
+				<tr class="list-pr-ap">
+					<td colspan="4" class="text-left">
+						<button class="btn prc-bill ">&laquo; Keep buying</button>
+					</td>
+					<td class="font-weight-bold">
+						Total: $2397
+					</td>
+					<td>
+						<button class="btn prc-bill">Pay &raquo;</button>
+					</td>
+				</tr>
+
+				<!-- Modal -->
+				<div class=" modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				  	<div class="modal-dialog" role="document">
+				    	<div class="modal-content">
+				      		<div class="modal-header">
+				        		<h5 class="modal-title text-transform">Confirm Delete</h5>
+				      		</div>
+				      		<div class="modal-body">
+				        		<h4 class="modal-title text-transform">Do you want to delete this row?</h4>
+				      		</div>
+					      	<div class="modal-footer">
+						        <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Back</button>
+						        <button type="button" class="yes-remove btn btn-danger btn-sm">Yes, please</button>
+					      	</div>
+				    	</div>
+				  	</div>
+				</div>
+
+			</tbody>
+		</table>
 		<table class="bg-cart bill-ap table table-inverse text-transform table-hover table-responsive">
 			<thead>
 				<tr>
