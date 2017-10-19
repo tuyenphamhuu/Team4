9b352159c5546de3cdb8443039b51fd0b5a0e2f9
NhungNTH95 <nhung.nth@neo-lab.vn>
2017-10-19 10:01:01 +0700

    * Fix bg-color mycart

--- app/views/mycart.view.php
+++ app/views/mycart.view.php
@@ -6,13 +6,12 @@
 
 	<!-- <?php var_dump($_SESSION['cart']); ?> -->
 		<h2 class="font-weight-bold text-center">My cart</h2>
-		<table class="bg-cart bill-ap table table-inverse text-transform table-hover table-responsive">
+		<table class="bg-cart bill-ap table text-transform table-hover table-responsive">
 			<thead>
 				<tr>
 					<!-- <th style="width: 4%">	#</th> -->
 					<th style="width: 8%">Images</th>
 					<th style="width: 27%">Name Products</th>
-					<th style="width: 10%">Storage</th>
 					<th style="width: 15%">Price</th>
 					<th style="width: 15%">Quantity</th>
 					<th style="width: 15%">Total Money</th>
@@ -25,18 +24,6 @@
 						<img class="img-myc" src="../../public/images/7+-f.jpg">
 					</td>
 					<td>iPhone 8</td>
-					<td>
-						<select>
-							<option>none</option>
-							<option>4GB</option>
-							<option>8GB</option>
-							<option>16GB</option>
-							<option>32GB</option>
-							<option>64GB</option>
-							<option>128GB</option>
-							<option>256GB</option>
-						</select>
-					</td>
 					<th>$699</th>
 					<td class="count-ap">
 						<input class="text-center" type="number" value="1" min="1" >
@@ -54,18 +41,6 @@
 						<img class="img-myc" src="../../public/images/ipad-pro-10in.png">
 					</td>
 					<td>iPad Pro 12.9 inch</td>
-					<td>
-						<select>
-							<option>none</option>
-							<option>4GB</option>
-							<option>8GB</option>
-							<option>16GB</option>
-							<option>32GB</option>
-							<option>64GB</option>
-							<option>128GB</option>
-							<option>256GB</option>
-						</select>
-					</td>
 					<th>$799</th>
 					<td class="count-ap">
 						<input class="text-center" type="number" value="2" min="1">
@@ -83,18 +58,6 @@
 						<img class="img-myc" src="../../public/images/6+.jpg">
 					</td>
 					<td>iPhone 7 plus</td>
-					<td>
-						<select>
-							<option>none</option>
-							<option>4GB</option>
-							<option>8GB</option>
-							<option>16GB</option>
-							<option>32GB</option>
-							<option>64GB</option>
-							<option>128GB</option>
-							<option>256GB</option>
-						</select>
-					</td>
 					<th>$669</th>
 					<td class="count-ap">
 						<input class="text-center" type="number" value="1" min="1">
@@ -108,7 +71,7 @@
 					</td>
 				</tr>
 				<tr class="list-pr-ap">
-					<td colspan="5" class="text-left">
+					<td colspan="4" class="text-left">
 						<button class="btn prc-bill ">&laquo; Keep buying</button>
 					</td>
 					<td class="font-weight-bold">
