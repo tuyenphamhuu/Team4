26db0beb833e5c0faa6282a21c8b7374052ff59e
NhungNTH95 <nhung.nth@neo-lab.vn>
2017-10-23 23:54:48 +0700

    * Fix 50% hover-products  in index.view.php page

--- app/views/detail-iPad.view.php
+++ app/views/detail-iPad.view.php
@@ -110,30 +110,20 @@
 			</div>
  		<div class="row">
                 <div class="row semore-product">
-       				<div class="col-md-3 text-center">
-            			<img class="img-fluid" src="public/images/ipad-pro-12in-256GB.png">
-            			<a class="colorblack" href="#">
-            				<p>12.9-inch iPad Pro 256GB</p>
-            			</a>
-           			</div>	
-        			<div class="col-md-3 text-center">
-            			<img class="img-fluid" src="public/images/ipad-9in-128GB.png">
-             			<a class="colorblack" href="#">
-             				<p>9.7-inch iPad Pro 128GB</p>
-             			</a>
-        			</div>
-    				<div class="col-md-3 text-center">	
-             			<img class="img-fluid" src="public/images/ipad-mini4-128G.png">
-            			<a class="colorblack" href="#">
-             				<p>iPad Mini 4 128GB</p>
-             			</a>
-        			</div>
-        			<div class="col-md-3 text-center">	
-             			<img class="img-fluid" src="public/images/ipad-pro-10in-256G.png">
-             			<a class="colorblack" href="#">
-            				<p>10.5 inch iPad Pro 256GB</p>
-             			</a>
-    				</div>
+                	<?php 
+                		foreach ($moreprd as $moreprds) {
+                			?>
+                				<div class="col-md-3 text-center">
+			            			<img class="img-fluid" src="public/images/<?php echo $moreprds->Image ?>">
+			            			<a class="colorblack" href="/detailProduct?idProduct=<?php echo $moreprds->ID_Product ?>&type=<?php echo $moreprds->ID_TypeProduct ?>">
+			            				<?php echo $moreprds->ProductName ?>
+			            			</a>
+			           			</div>
+                			<?php
+                		}
+                	 ?>
+       					
+        			
       			</div>
  			</div>
  		</div>
--- app/views/index.view.php
+++ app/views/index.view.php
@@ -9,7 +9,7 @@
 				<div class="container-fluid margin-lr">
 					<div class="row justify-content-center prd">
 						<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="wrap-item-prd hover ehover3">
+							<div class="wrap-item-prd hover-ap hover">
 						    <img class="mx-auto d-block rounded" src="public/images/ipx.png" alt="">
 						    <div class="overlay-n">
 						      <h2>iPhone X</h2>
@@ -18,7 +18,7 @@
 						  </div>
 						</div>
 						<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="wrap-item-prd hover ehover3">
+							<div class="wrap-item-prd hover-ap hover">
 								<img class="mx-auto d-block rounded" src="public/images/ip8-.jpg" alt="iphone_8">
 								<div class="overlay-n">
 									<h2>Iphone 8</h2>
@@ -29,7 +29,7 @@
 							</div>
 						</div>
 						<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="wrap-item-prd hover ehover3">
+							<div class="wrap-item-prd hover-ap hover">
 								<img class="mx-auto d-block rounded" src="public/images/ip7-.jpg" alt="iphone_7">
 								<div class="overlay-n">
 									<h2>Iphone 7</h2>
@@ -40,7 +40,7 @@
 							</div>
 						</div>
 						<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="wrap-item-prd hover ehover3">
+							<div class="wrap-item-prd hover-ap hover">
 								<img class="mx-auto d-block rounded" src="public/images/ip6s-.gif" alt="iphone_6">
 								<div class="overlay-n">
 									<h2>Iphone 6s</h2>
@@ -61,7 +61,7 @@
 				<div class="container-fluid margin-lr">
 					<div class="row prd">
 						<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="wrap-item-prd hover ehover3">
+							<div class="wrap-item-prd hover-ap hover">
 								<img class="mx-auto d-block rounded" src="public/images/ipad-pro-f.jpg" alt="ipad-pro-f.jpg">
 								<div class="overlay-n">
 									<h2>Ipad Pro</h2>
@@ -72,7 +72,7 @@
 							</div>
 						</div>
 						<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="wrap-item-prd hover ehover3">
+							<div class="wrap-item-prd hover-ap hover">
 								<img class="mx-auto d-block rounded" src="public/images/ipad-f.jpg" alt="ipad-f.jpg">
 								<div class="overlay-n">
 									<h2>Ipad</h2>
@@ -83,7 +83,7 @@
 							</div>
 						</div>
 						<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="wrap-item-prd hover ehover3">
+							<div class="wrap-item-prd hover-ap hover">
 								<img class="mx-auto d-block rounded" src="public/images/ipad-mini-4-f.jpg" alt="ipad-mini-4-f.jpg">
 								<div class="overlay-n">
 									<h2>Ipad mini 4</h2>
@@ -94,7 +94,7 @@
 							</div>
 						</div>
 						<div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12">
-							<div class="wrap-item-prd hover ehover3">
+							<div class="wrap-item-prd hover-ap hover">
 								<img class="mx-auto d-block rounded" src="public/images/ios11-f.jpg" alt="ios11-f.jpg">
 								<div class="overlay-n">
 									<h2>iOS 11</h2>
