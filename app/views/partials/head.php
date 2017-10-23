b05dd151be87f0cfd23997db3d9512bfae07f91f
NhungNTH95 <nhung.nth@neo-lab.vn>
2017-10-23 07:57:47 +0700

    new week pull

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -19,6 +19,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" ></script> <!-- modal -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
 	<script src="/public/js/custom.js" type="text/javascript" charset="utf-8" async defer></script>
+	<script type="text/javascript" charset="utf-8" src="/public/js/n-effect.js"></script>
 
 	<!-- <script type="text/javascript" charset="utf-8" src="/public.js/jquery-3.1.1.slim.min.js"></script> -->
 	<!-- 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
