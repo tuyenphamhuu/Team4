83868b408aa52483d38e48e306ebb5d403a37bdb
NhungNTH95 <nhung.nth@neo-lab.vn>
2017-10-25 13:42:10 +0700

    ? [13:42 25/10]

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -12,20 +12,19 @@
 	<link rel="stylesheet" href="/public/css/reponsive.css">
 	<link rel="stylesheet" href="/public/css/jquery.dataTables.css">
 
-
 	<link rel="stylesheet" type="text/css" href="/public/css/effects.min.css">
 	<link rel="stylesheet" type="text/css" href="/public/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/public/css/bootstrap2.min.css">
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/	twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css">
+
 	<script type="text/javascript" charset="utf-8" src="/public/js/jquery-3.2.1.min.js"></script>	
 	<script type="text/javascript" charset="utf-8" src="/public.js/jquery-3.1.1.slim.min.js"></script>
 	<script type="text/javascript" charset="utf-8" src="/public/js/bootstrap.min.js" ></script>
 	<script type="text/javascript" charset="utf-8" src="/public/js/bootstrap2.min.js"></script>
 	<script type="text/javascript" charset="utf-8" src="/public/js/jquery.dataTables.min.js"></script>
 	<script src="/public/js/custom.js" type="text/javascript" charset="utf-8" async defer></script>
-<!-- >>>>>>> 980614f4ccb962487eb9e3799e7569887b59e5a2 -->
-	<script src="/public/js/custom.js" charset="utf-8" async defer></script>
 
+	<script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
 	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script> -->
