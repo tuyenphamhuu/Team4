b1433ad2f1659b7ee3dd6475c4472a5540bbac5c
Hang Vo <hang.vt@neolab.vn>
2017-10-25 16:12:45 +0700

    *fix topmenu sidebar

--- app/views/partials/sidebar-left.php
+++ app/views/partials/sidebar-left.php
@@ -44,7 +44,7 @@
 	   		</div>
     </div>
     <hr>
-	<nav class="navbar navbar-expand-lg">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
     	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
     		<span class="navbar-toggler-icon"></span>
   		</button>		
