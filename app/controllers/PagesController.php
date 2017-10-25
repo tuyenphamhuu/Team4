c3e18893c8d49fcc3700e907474d7d522cfb434f
Phạm Hữu Tuyến <tuyen.ph@neo-lab.vn>
2017-10-25 13:42:49 +0700

    * index.view.php pagecontroller-> seach

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -100,7 +100,7 @@ class PagesController
   {
     $name     = $_POST['aname'];
     $sql      = " SELECT * FROM product WHERE ProductName LIKE '%{$name}%' ORDER BY ID_Product DESC ";
-    $results  = Product::selectByType($sql); 
+    $results  = Product::selectByType($sql);
     return view('seach', ['results' => $results]);    
   }
 }
